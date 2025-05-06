4eb5dae940ef6a11f965429696089e9f1117ae2e
BekaUngo <beqaungiadze@gmail.com>
2025-05-06 18:58:08 +0400

    remove extra files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BtnComponent } from './btn/btn.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { SideBarDetailsComponent } from './side-bar/side-bar-details/side-bar-details.component';
-import { CarsComponent } from './cars/cars/cars.component';
-import { CarDetailsComponent } from './cars/cars/car-details/car-details.component';
 import { MainComponent } from './main/main.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutUsComponent } from './about-us/about-us.component';
@@ -28,12 +25,9 @@ import { TestFormComponent } from './test-form/test-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BtnComponent,
     HeaderComponent,
     SideBarComponent,
     SideBarDetailsComponent,
-    CarsComponent,
-    CarDetailsComponent,
     MainComponent,
     ContactComponent,
     AboutUsComponent,
